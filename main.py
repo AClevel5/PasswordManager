0e9d76226472e507b485f52ea4144f02e69e099d
alexcleveland <alexcleveland5@gmail.com>
2026-01-29 19:01:41 -0700

    Added all Entries, Labels, and Buttons

--- main.py
+++ main.py
@@ -6,9 +6,26 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100,image=lock_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+website_label = Label(window, text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(window, text="Email")
+email_label.grid(row=2, column=0)
+pass_label = Label(window, text="Password")
+pass_label.grid(row=3, column=0)
 
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+pass_entry = Entry(width=20)
+pass_entry.grid(row=3, column=1)
+
+generate_password_button = Button(text="Generate Password", width=11)
+generate_password_button.grid(row=3, column=2)
+add_password_button = Button(text="Add Password", width=33)
+add_password_button.grid(row=4, column=1, columnspan=2)
 
 
 
